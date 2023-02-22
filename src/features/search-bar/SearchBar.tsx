e4d614154f512b5b5ead235af24d058ca2b7ad52
AlexaDavidenko <agarkova.al@yandex.ru>
2023-02-22 18:51:57 +0300

    search placeholder

--- src/features/search-bar/SearchBar.tsx
+++ src/features/search-bar/SearchBar.tsx
@@ -23,7 +23,7 @@ export const SearchBar = () => {
         <>
             <div className={styles.wrapper}>
                 <form onSubmit={onSubmit}>
-                    <input type="text" className={styles.search} value={inputValue} onChange={(e) => setValue(e.target.value)}/>
+                    <input type="text" placeholder="Search..." className={styles.search} value={inputValue} onChange={(e) => setValue(e.target.value)}/>
                     <button className={styles.searchButton}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
                 </form>
             </div>
